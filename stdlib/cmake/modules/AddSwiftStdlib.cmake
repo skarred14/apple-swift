3ff3d206ccebe8af68dd2bb0467700676f5c2308
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-09-11 23:34:46 -0700

    Add i686 and x86_64 to allowed android architecture list.

--- stdlib/cmake/modules/AddSwiftStdlib.cmake
+++ stdlib/cmake/modules/AddSwiftStdlib.cmake
@@ -291,7 +291,7 @@ function(_add_target_variant_c_compile_flags)
                        "-fcoverage-mapping")
   endif()
 
-  if((CFLAGS_ARCH STREQUAL "armv7" OR CFLAGS_ARCH STREQUAL "aarch64") AND
+  if((CFLAGS_ARCH STREQUAL "armv7" OR CFLAGS_ARCH STREQUAL "aarch64" OR CFLAGS_ARCH STREQUAL "i686" OR CFLAGS_ARCH STREQUAL "x86_64") AND
      (CFLAGS_SDK STREQUAL "LINUX" OR CFLAGS_SDK STREQUAL "ANDROID"))
      list(APPEND result -funwind-tables)
   endif()
